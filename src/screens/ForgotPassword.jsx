05047908c313ea8d3b9b3842533d91b143c8f768
Ayouboss02 <138698902+Ayouboss02@users.noreply.github.com>
2024-12-19 00:47:25 +0100

    ajout de la page forgot password

--- src/screens/ForgotPassword.jsx
+++ src/screens/ForgotPassword.jsx
@@ -1,14 +1,79 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, Keyboard, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Color } from '../../constants/Color'
+import { Controller, useForm } from 'react-hook-form'
 
-const ForgotPassword = () => {
+const ForgotPassword = ({ navigation }) => {
+
+    const {
+        control,
+        handleSubmit,
+        formState: {
+            errors
+        }
+    } = useForm();
+
+    const handleBackToLogin = () => {
+        navigation.navigate('login');
+    }
+
+    const submit = async (data) => {
+        try {
+            const response = await axios.post('/reset-password', data);
+            navigation.navigate('checkEmail', {data})
+            console.log(response.data);
+            alert('reset code sent');
+        } catch (error) {
+            console.error('Error sending appointment request:', error);
+            alert("Erreur lors de reset password");
+        }
+    };
+
     return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.headerContainer}>
-                <Text style={styles.title}>Forgot Password?</Text>
-                <Text style={styles.subtitle}>To reset your password, you need your email or mobile number that can be authenticated</Text>
-            </View>
+            <StatusBar barStyle="dark-content" backgroundColor={Color.background} />
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View>
+                    <View style={styles.headerContainer}>
+                        <Text style={styles.title}>Forgot Password?</Text>
+                        <Text style={[styles.subtitle, {lineHeight:20}]}>To reset your password, you need your email or mobile number that can be authenticated</Text>
+                    </View>
+                    <Image source={require('../../assets/forgotpassword.png')} style={styles.image}/>
+                    <Text style={styles.inputTitle}>Email</Text>
+                    <Controller
+                        name='email'
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                placeholder='Brandonelouis@gmail.com'
+                                placeholderTextColor={Color.placeholderText}
+                                value={value}
+                                style={[styles.textInput, value && { fontWeight: "600" }]}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                keyboardType='email-address'
+                                autoComplete='email'
+                            />
+                        )}
+                        rules={{
+                            required: true, pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "Votre email pas correct"
+                            }
+                        }}
+                    />
+                    {errors?.email?.type === "required" && <Text style={styles.errorText}>Veuillez saisir votre email</Text>}
+                    {errors?.email?.type === "pattern" && <Text style={styles.errorText}>{errors?.email?.message}</Text>}
+
+                    <TouchableOpacity style={styles.resetButton} onPress={handleSubmit(submit)}>
+                        <Text style={styles.resetText}>RESET PASSWORD</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.backLoginButton} onPress={handleBackToLogin}>
+                        <Text style={styles.backLoginText}>BACK TO LOGIN</Text>
+                    </TouchableOpacity>
+                </View>
+            </TouchableWithoutFeedback>
         </SafeAreaView>
     )
 }
@@ -18,21 +83,68 @@ export default ForgotPassword
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingVertical: 10,
         alignItems: "center",
         justifyContent: "center",
+        backgroundColor: Color.background,
     },
     headerContainer: {
-        paddingVertical: 30,
+        padding: 20,
         alignItems: "center",
     },
     title: {
         fontWeight: '700',
-        fontSize: 35,
-        padding: 10,
+        fontSize: 30,
+        padding: 20,
+        color: Color.text,
     },
     subtitle: {
-        fontSize: 16,
+        fontSize: 12,
+        fontWeight: '400',
         textAlign: "center",
+        color: Color.subtitle,
+    },
+    inputTitle: {
+        fontSize: 12,
+        fontWeight: '700',
+        color: Color.text,
+    },
+    textInput: {
+        marginVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+        height: 50,
+    },
+    errorText: {
+        color: "red"
+    },
+    resetButton: {
+        backgroundColor: Color.selectedbutton,
+        margin: 20,
+        paddingVertical: 20,
+        borderRadius: 10,
+    },
+    resetText: {
+        textAlign: 'center',
+        color: "#ffffff",
+        fontWeight: "700",
+        fontSize: 14
+    },
+    backLoginButton: {
+        backgroundColor: Color.unselectedbutton,
+        marginHorizontal: 20,
+        paddingVertical: 20,
+        borderRadius: 10,
+    },
+    backLoginText: {
+        textAlign: 'center',
+        color: "#ffffff",
+        fontWeight: "700",
+        fontSize: 14
+    },
+    image: {
+        alignSelf:'center',
+        padding:10,
+        margin:60,
     },
 })
