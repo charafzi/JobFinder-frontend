0814e210c75bd368695e344d886042e12d40e0a1
charafzi <ghaziachraf99@gmail.com>
2025-01-01 21:25:08 +0100

    Moving ForgotPassword API handling to redux

---
+++ src/redux/reducers/forgotPasswordReducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+    isLoading: false,
+    mailIsSent: false,
+    error: null
+};
+
+const forgotPasswordReducer = (state = initialState, action ) =>{
+    switch (action.type) {
+        case 'FORGOTPASSWORD_REQUEST':
+            return { ...state, isLoading: true, error: null };
+        case 'FORGOTPASSWORD_SUCCESS':
+            return { ...state, isLoading: false, mailIsSent: true};
+        case 'FORGOTPASSWORD_FAILURE':
+            return { ...state, isLoading: false, mailIsSent: false, error: action.payload};
+        default:
+            return state;
+    }
+}
+
+export default forgotPasswordReducer;
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import AuthReducer from "./reducers/authReducer";
+import forgotPasswordReducer from "./reducers/forgotPasswordReducer";
 
 const rootReducer = combineReducers({
     auth : AuthReducer,
+    forgotPassword : forgotPasswordReducer
 })
 
 export default rootReducer;
