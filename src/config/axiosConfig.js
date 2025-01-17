70bc158e8e9f353b60a924d82817e52783ad9be3
charafzi <ghaziachraf99@gmail.com>
2025-01-17 10:52:26 +0100

    Migration de Managed Workflow vers Bare Workflow

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Platform } from "react-native";
 
 const isAndroid = Platform.OS === "android";
-const localhost = isAndroid ? "192.168.1.2" : "localhost";
+const localhost = isAndroid ? "192.168.0.104" : "localhost";
 
 export const API_BASE_URL = `http://${localhost}:8091`;
 export const WEBSOCKETIO_URL = `http://${localhost}:8092`;
