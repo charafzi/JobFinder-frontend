614c10c424a48d14784ed2465159e92974c18e90
Ayouboss02 <138698902+Ayouboss02@users.noreply.github.com>
2025-01-19 21:50:19 +0100

    Save local changes to axiosConfig.js

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const localhost = "192.168.1.5";
+const localhost = "192.168.1.30";
 
 export const API_BASE_URL = `http://${localhost}:8091`;
 export const WEBSOCKETIO_URL = `http://${localhost}:8092`;
