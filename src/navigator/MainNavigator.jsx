9f7afc0890ea5ca65dc292db4140aae0a28c9f0b
Ayouboss02 <138698902+Ayouboss02@users.noreply.github.com>
2025-01-21 05:58:07 +0100

    resolving conflits of merge with achraf-branch

--- src/navigator/MainNavigator.jsx
+++ src/navigator/MainNavigator.jsx
@@ -10,20 +10,15 @@ import {
   Register,
   ResetSuccessfully,
   Welcome,
-  Map, Search, Filter,
-    EntrepriseHomeScreen, ApplicationApply, ApplicationDetails, Applications,
-  EntrepriseJobDetailsScreen,
-  EntrepriseCandidates,
-  Map, Search, Filter,
+  EntrepriseHomeScreen, ApplicationApply, ApplicationDetails, Applications,
 } from "../screens";
 import TabNavigator from "./BottomTabNavigator";
 import CompanyProfile from "../screens/CompanyProfile";
-import EditCompanyProfile from '../screens/EditCompanyProfile';
 import CandidatProfile from '../screens/CandidatProfile';
 import UploadCV from '../screens/UploadCV';
 import UploadCVSuccess from '../screens/UploadCVSuccess';
 import EditProfileCandidat from '../screens/EditProfileCandidat';
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 
 const Stack = createNativeStackNavigator();
 
@@ -32,28 +27,25 @@ const StackNavigator = () => {
 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={isLoggedIn ? "tabNavigator" : "login"} >
-        <Stack.Screen name="tabNavigator" component={TabNavigator} />
-        <Stack.Screen name="logoscreen" component={LogoScreen} />
-        <Stack.Screen name="welcome" component={Welcome} />
-        <Stack.Screen name="login" component={Login} />
-        <Stack.Screen name="register" component={Register} />
-        <Stack.Screen name="forgotpassword" component={ForgotPassword} />
-        <Stack.Screen name="checkEmail" component={CheckEmail} />
-        <Stack.Screen name="newPassword" component={NewPassword} />
-        <Stack.Screen name="resetSuccessfully" component={ResetSuccessfully} />
-        <Stack.Screen name={"Map"} component={Map}/>
-        <Stack.Screen name={"Search"} component={Search}></Stack.Screen>
-        <Stack.Screen name={"Filter"} component={Filter}></Stack.Screen>
-        <Stack.Screen name="jobPreview" component={JobPreview} />
-        <Stack.Screen name="applications" component={Applications}></Stack.Screen>
-        <Stack.Screen name="applicationDetails" component={ApplicationDetails}></Stack.Screen>
-        <Stack.Screen name="applicationApply" component={ApplicationApply}></Stack.Screen>
-        <Stack.Screen name="UploadCV" component={UploadCV} />
-        <Stack.Screen name="UploadCVSuccess" component={UploadCVSuccess} />
-        <Stack.Screen name="EditProfileCandidat" component={EditProfileCandidat} />
-        <Stack.Screen name={"entreprise"} component={EntrepriseHomeScreen}></Stack.Screen>
-        <Stack.Screen name={"company"} component={CompanyProfile}></Stack.Screen>
-        <Stack.Screen name={"candidat"} component={CandidatProfile}></Stack.Screen>
+      <Stack.Screen name="tabNavigator" component={TabNavigator} />
+      <Stack.Screen name="logoscreen" component={LogoScreen} />
+      <Stack.Screen name="welcome" component={Welcome} />
+      <Stack.Screen name="login" component={Login} />
+      <Stack.Screen name="register" component={Register} />
+      <Stack.Screen name="forgotpassword" component={ForgotPassword} />
+      <Stack.Screen name="checkEmail" component={CheckEmail} />
+      <Stack.Screen name="newPassword" component={NewPassword} />
+      <Stack.Screen name="resetSuccessfully" component={ResetSuccessfully} />
+      <Stack.Screen name="jobPreview" component={JobPreview} />
+      <Stack.Screen name="applications" component={Applications}></Stack.Screen>
+      <Stack.Screen name="applicationDetails" component={ApplicationDetails}></Stack.Screen>
+      <Stack.Screen name="applicationApply" component={ApplicationApply}></Stack.Screen>
+      <Stack.Screen name="UploadCV" component={UploadCV} />
+      <Stack.Screen name="UploadCVSuccess" component={UploadCVSuccess} />
+      <Stack.Screen name="EditProfileCandidat" component={EditProfileCandidat} />
+      <Stack.Screen name={"entreprise"} component={EntrepriseHomeScreen}></Stack.Screen>
+      <Stack.Screen name={"company"} component={CompanyProfile}></Stack.Screen>
+      <Stack.Screen name={"candidat"} component={CandidatProfile}></Stack.Screen>
     </Stack.Navigator>
   );
 };
